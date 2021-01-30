9fbec5d1abd2374bcd68372d2d95118409c41e25
Bing <odowd.bing@gmail.com>
2021-01-30 16:00:40 +0100

    minimal working example

--- Enumerate.py
+++ Enumerate.py
@@ -3,7 +3,7 @@ from rdkit.Chem import AllChem
 from typing import List
 import itertools
 
-def enumerate_mols(core: List[List[str]], chain: List[List[str]], rxn_smarts: str = '[c:1][#0].[#0][*:2]>>[c:1]-[*:2]' ):
+def enumerate_mols(cores: List[List[str]], chains: List[List[str]], rxn_smarts: str = '[c:1][#0].[#0][*:2]>>[c:1]-[*:2]' ):
     """
 
     :param core: List of lists [[ID, SMILES], [ID, SMILES],...]
@@ -13,7 +13,11 @@ def enumerate_mols(core: List[List[str]], chain: List[List[str]], rxn_smarts: st
     """
     rxn = AllChem.ReactionFromSmarts(rxn_smarts)
     res = []
-    combos = itertools.product()
+
+    # don't use dict for cores, chains. Product will use the keys and overwrite if there are duplicate keys.
+    # there are usecases where you want duplicate keys. e.g. to track one building block in many different mols
+    combos = itertools.product(cores, chains)
+
     for t in combos:
         cur_core_id = t[0][0]
         cur_core_smi = t[0][1]
@@ -22,7 +26,7 @@ def enumerate_mols(core: List[List[str]], chain: List[List[str]], rxn_smarts: st
         cur_chain_id = t[1][0]
         cur_chain_smi = t[1][1]
         cur_chain_mol = Chem.MolFromSmiles(cur_chain_smi)
-        p = rxn.RunReactants((cur_core_mol, cur_core_smi))
+        p = rxn.RunReactants((cur_core_mol, cur_core_mol))
         product = Chem.MolToSmiles(p[0][0])
 
     res.append([cur_core_id, cur_core_mol, cur_chain_id, cur_chain_mol, product])
@@ -36,3 +40,4 @@ def replace_dummy_with_star(smi_list: List = None, mol_list: List = None, dummy:
     else:
         return 'Please input a list of smiles or list of mol objects'
 
+# print(enumerate_mols(cores=[['ID1', '*c1c(C)cccc1(O)']], chains=[['ID2', 'CN*']]))
